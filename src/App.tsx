bb1d37750a9695576245de4a2c0dc2de1196fe5e
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-02-23 21:28:47 +0100

    build navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Vitals from "./components/Vitals";
+import NavBar from "./components/AppBar";
 import "./static/App.css";
 
 function App() {
 	return (
 		<div className="App">
+			<NavBar />
 			<header className="App-header">
 				<p>MIMICView</p>
 				<p>Visualize MIMIC data</p>
